e43f3000e51ede7376e5d727285a46c1eb2ff5bc
dengwenj <927561237@qq.com>
2023-02-22 21:07:35 +0800

    定义用户管理api

--- src/services/user/index.ts
+++ src/services/user/index.ts
@@ -1,5 +1,38 @@
 import { apiPost } from "..";
 
+// 登录
 export const login = (data: {username: string, password: string}) => {
   return apiPost('/user/login', data);
 }
+
+// 管理员(增删改查)
+export const addAdmin = (data: Record<string, any>) => {
+  return apiPost('/user/add', data);
+};
+export const deleteAdmin = (id: number) => {
+  return apiPost('/user/delete', { id });
+};
+export const editAdmin = (data: Record<string, any>) => {
+  return apiPost('/user/update', data);
+};
+export const pageAdmin = (data: Record<string, any>) => {
+  return apiPost('/user/page', data);
+};
+
+// 人员管理(增删改查)
+export const addOldPerson = (data: Record<string, any>) => {
+  return apiPost('/oldperson/add', data);
+};
+export const deleteOldPerson = (id: number) => {
+  return apiPost('/oldperson/delete', { id });
+};
+export const editOldPerson = (data: Record<string, any>) => {
+  return apiPost('/oldperson/update', data);
+};
+export const pageOldPerson = (data: Record<string, any>) => {
+  return apiPost('/oldperson/page', data);
+};
+// 通过人员获取人员列表
+export const getOldpersonByName = (oldPersonName: string) => {
+  return apiPost('/oldperson/getOldpersonByName', { oldPersonName });
+};
