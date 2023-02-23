bcf374aa508abf47f92407d8fe8176379b36105c
dengwenj <927561237@qq.com>
2023-02-23 20:19:10 +0800

    layout修改

--- src/page/Layout/index.tsx
+++ src/page/Layout/index.tsx
@@ -1,159 +1,111 @@
-import React, { useState } from 'react';
-import { useNavigate, Outlet, useLocation } from 'react-router-dom';
+import { useState } from 'react';
+import { Dropdown, message, Tag } from 'antd';
+import { PageContainer, ProLayout } from '@ant-design/pro-components';
+import { Outlet } from 'react-router-dom';
 import {
-  RightOutlined,
-  LeftOutlined,
-  UserOutlined,
-  LogoutOutlined
+  LogoutOutlined,
+  InfoCircleFilled,
+  QuestionCircleFilled,
+  GithubFilled,
 } from '@ant-design/icons';
-import { Avatar, Dropdown, Breadcrumb, Layout, Menu, message } from 'antd';
+import { useNavigate, useLocation } from 'react-router-dom';
 
-import items, { pathLabel } from './route';
+import route from './route';
 import logo from '../../access/imgs/logo.png';
-import './index.css';
 
-const { Header, Content, Footer, Sider } = Layout;
-
-const MyLayout: React.FC = () => {
-  const [collapsed, setCollapsed] = useState(false);
+export default () => {
+  const location = useLocation;
+  const [pathname, setPathname] = useState(location().pathname);
   const navigate = useNavigate();
-  const location = useLocation();
-  const [keyPath, setKeyPath] = useState<string[]>([location.pathname, `/${location.pathname.split('/')[1]}`]);
-
 
   return (
-    <Layout style={{ height: '100%' }}>
-      <Sider
-        style={{
-          backgroundColor: '#fff',
-          position: 'relative',
+    <div
+      id="test-pro-layout"
+      style={{
+        height: '100vh',
+      }}
+    >
+      <ProLayout
+        siderWidth={208}
+        layout="mix"
+        headerContentRender={() => {
+          return (
+            <div style={{ marginLeft: 20 }}>
+              <Tag color='#00b96b'>海纳百川，有容乃大</Tag>
+            </div>
+          )
         }}
-        collapsible
-        collapsed={collapsed}
-        width={208}
-        trigger={null}
-      >
-        <div
-          className='trigger'
-          onClick={() => {
-            setCollapsed(!collapsed)
-          }}
-        >
-          {collapsed ? <RightOutlined /> : <LeftOutlined />}
-        </div>
-
-        <div style={{
-          position: 'absolute',
-          padding: 15,
-          display: 'flex',
-          alignItems: 'center',
-        }}>
-          <img src={logo} width={40} />
-          {
-            !collapsed && <h1 style={{ marginLeft: 10, fontSize: 16, }}>养老院系统设计</h1>
+        route={route}
+        menuProps={{
+          onClick(info) {
+            setPathname(info.key)
+            navigate(info.key)
           }
-        </div>
-        <div
-          style={{
-            position: 'absolute',
-            top: 63,
-            height: 1,
-            width: '100%',
-            borderBottom: '1px solid #f5f5f5'
-          }}
-        />
-        <Menu
-          style={{ paddingTop: 65, height: '100%' }}
-          theme='light'
-          mode='inline'
-          defaultSelectedKeys={[location.pathname]}
-          defaultOpenKeys={[`/${location.pathname.split('/')[1]}`]}
-          items={items}
-          onClick={(info) => {
-            setKeyPath(info.keyPath);
-            navigate(info.key);
-            const label = pathLabel.find((item) => item.path === info.key)?.label;
-            document.title = `${label || ''} - 养老院系统`;
-          }}
-        />
-        <Footer
-          style={{
-            textAlign: 'center',
-            position: 'absolute',
-            padding: '20px 0',
-            bottom: 0,
-            width: '100%',
-            backgroundColor: '#fff',
-          }}>©2023 养老院系统设计</Footer>
-      </Sider>
-      <Layout>
-        <Header
-          style={{
-            padding: '0 23px',
-            backgroundColor: '#fff',
-            display: 'flex',
-            justifyContent: 'flex-end'
-          }}
-        >
-          <div
-            style={{
-              display: 'flex',
-              justifyContent: 'space-between',
-              width: '100%',
-              alignItems: 'center'
-            }}
-          >
-            <Breadcrumb>
-              {
-                keyPath.reverse().map((item) => {
-                  const res = pathLabel.find((itex) => item === itex.path);
-                  return (
-                    <Breadcrumb.Item key={item}>{res?.label}</Breadcrumb.Item>
-                  )
-                })
-              }
-            </Breadcrumb>
-
-            <Dropdown
-              menu={{
-                items: [
-                  {
-                    key: 'logout',
-                    icon: <LogoutOutlined />,
-                    label: '退出登录',
-                    onClick() {
-                      navigate('/user/login');
-                      localStorage.removeItem('old_person_home_token');
-                      localStorage.removeItem('o_p_h_user_info');
-                      message.info('登出成功');
-                    }
-                  },
-                ],
+        }}
+        location={{
+          pathname,
+        }}
+        avatarProps={{
+          src: 'https://joesch.moe/api/v1/random',
+          title: JSON.parse(localStorage.getItem('o_p_h_user_info') || '')?.username,
+          size: 'small',
+          render: (props, dom) => {
+            return (
+              <Dropdown
+                menu={{
+                  items: [
+                    {
+                      key: 'logout',
+                      icon: <LogoutOutlined />,
+                      label: '退出登录',
+                      onClick() {
+                        navigate('/user/login');
+                        localStorage.removeItem('old_person_home_token');
+                        localStorage.removeItem('o_p_h_user_info');
+                        message.info('登出成功');
+                      }
+                    },
+                  ],
+                }}
+              >
+                {dom}
+              </Dropdown>
+            );
+          },
+        }}
+        title="养老院系统设计"
+        logo={logo}
+        actionsRender={(props) => {
+          if (props.isMobile) return [];
+          return [
+            <InfoCircleFilled key="InfoCircleFilled" />,
+            <QuestionCircleFilled key="QuestionCircleFilled" />,
+            <GithubFilled key="GithubFilled" />,
+          ];
+        }}
+        menuFooterRender={(props) => {
+          return (
+            <div
+              style={{
+                textAlign: 'center',
+                paddingBlockStart: 12,
               }}
             >
-              <div style={{ cursor: 'pointer' }}>
-                <Avatar
-                  src='https://joesch.moe/api/v1/random'
-                  size='default'
-                  icon={<UserOutlined />}
-                />
-                <span style={{ marginLeft: 10 }}>
-                  {
-                    JSON.parse(localStorage.getItem('o_p_h_user_info') || '')?.username
-                  }
-                </span>
-              </div>
-            </Dropdown>
-          </div>
-        </Header>
-        <Content style={{}}>
-          <div style={{ padding: 24, minHeight: 360, }}>
-            <Outlet />
-          </div>
-        </Content>
-      </Layout >
-    </Layout >
+              <div>© 2023 养老社区设计</div>
+            </div>
+          );
+        }}
+      >
+        <PageContainer
+          header={{
+            style: {
+              backgroundColor: '#fff',
+            }
+          }}
+        >
+          <Outlet />
+        </PageContainer>
+      </ProLayout>
+    </div>
   );
 };
-
-export default MyLayout;
--- src/page/Layout/route.tsx
+++ src/page/Layout/route.tsx
@@ -8,160 +8,93 @@ import {
   SolutionOutlined
 } from '@ant-design/icons';
 
-export default [
-  {
-    key: '/home',
-    icon: <SmileOutlined />,
-    label: '工作台',
-  },
-  {
-    key: '/user',
-    icon: <UserOutlined />,
-    label: '用户管理',
-    children: [
-      {
-        key: '/user/admin',
-        label: '账号管理'
-      },
-      {
-        key: '/user/oldperson',
-        label: '人员管理'
-      }
-    ]
-  },
-  {
-    key: '/healthy',
-    icon: <FieldTimeOutlined />,
-    label: '健康管理',
-    children: [
-      {
-        key: '/healthy/file',
-        label: '健康档案'
-      },
-      {
-        key: '/healthy/cases',
-        label: '病历档案'
-      }
-    ]
-  },
-  {
-    key: '/life',
-    icon: <CoffeeOutlined />,
-    label: '生活管理',
-    children: [
-      {
-        key: '/life/workrest',
-        label: '作息计划'
-      },
-      {
-        key: '/life/goout',
-        label: '外出报备'
-      }
-    ]
-  },
-  {
-    key: '/checkin',
-    icon: <HomeOutlined />,
-    label: '入住管理',
-    children: [
-      {
-        key: '/checkin/register',
-        label: '入住登记'
-      },
-      {
-        key: '/checkin/bedroom',
-        label: '寝室管理'
-      }
-    ]
-  },
-  {
-    key: '/safety',
-    icon: <ApiOutlined />,
-    label: '安全管理',
-    children: [
-      {
-        key: '/safety/accident',
-        label: '事故记录'
-      },
-      {
-        key: '/safety/visitors',
-        label: '访客记录'
-      }
-    ]
-  },
-  {
-    key: '/careworker',
-    icon: <SolutionOutlined />,
-    label: '护工管理',
-  },
-]
-export const pathLabel: { path: string, label: string }[] = [
-  {
-    path: '/home',
-    label: '工作台'
-  },
-  {
-    path: '/user',
-    label: '用户管理'
-  },
-  {
-    path: '/user/admin',
-    label: '账号管理'
-  },
-  {
-    path: '/user/oldperson',
-    label: '人员管理'
-  },
-  {
-    path: '/healthy',
-    label: '健康管理'
-  },
-  {
-    path: '/healthy/file',
-    label: '健康档案'
-  },
-  {
-    path: '/healthy/cases',
-    label: '病历档案'
-  },
-  {
-    path: '/life',
-    label: '生活管理'
-  },
-  {
-    path: '/life/workrest',
-    label: '作息计划'
-  },
-  {
-    path: '/life/goout',
-    label: '外出报备'
-  },
-  {
-    path: '/checkin',
-    label: '入住管理'
-  },
-  {
-    path: '/checkin/register',
-    label: '入住登记'
-  },
-  {
-    path: '/checkin/bedroom',
-    label: '寝室管理'
-  },
-  {
-    path: '/safety',
-    label: '安全管理'
-  },
-  {
-    path: '/safety/accident',
-    label: '事故管理'
-  },
-  {
-    path: '/safety/visitors',
-    label: '访客管理'
-  },
-  {
-    path: '/careworker',
-    label: '护工管理'
-  },
-];
+export default {
+  path: '/',
+  children: [
+    {
+      path: '/home',
+      icon: <SmileOutlined />,
+      name: '工作台',
+    },
+    {
+      path: '/user',
+      icon: <UserOutlined />,
+      name: '用户管理',
+      children: [
+        {
+          path: '/user/admin',
+          name: '账号管理'
+        },
+        {
+          path: '/user/oldperson',
+          name: '人员管理'
+        }
+      ]
+    },
+    {
+      path: '/healthy',
+      icon: <FieldTimeOutlined />,
+      name: '健康管理',
+      children: [
+        {
+          path: '/healthy/file',
+          name: '健康档案'
+        },
+        {
+          path: '/healthy/cases',
+          name: '病历档案'
+        }
+      ]
+    },
+    {
+      path: '/life',
+      icon: <CoffeeOutlined />,
+      name: '生活管理',
+      children: [
+        {
+          path: '/life/workrest',
+          name: '作息计划'
+        },
+        {
+          path: '/life/goout',
+          name: '外出报备'
+        }
+      ]
+    },
+    {
+      path: '/checkin',
+      icon: <HomeOutlined />,
+      name: '入住管理',
+      children: [
+        {
+          path: '/checkin/register',
+          name: '入住登记'
+        },
+        {
+          path: '/checkin/bedroom',
+          name: '寝室管理'
+        }
+      ]
+    },
+    {
+      path: '/safety',
+      icon: <ApiOutlined />,
+      name: '安全管理',
+      children: [
+        {
+          path: '/safety/accident',
+          name: '事故记录'
+        },
+        {
+          path: '/safety/visitors',
+          name: '访客记录'
+        }
+      ]
+    },
+    {
+      path: '/careworker',
+      icon: <SolutionOutlined />,
+      name: '护工管理',
+    },
+  ]
+}
