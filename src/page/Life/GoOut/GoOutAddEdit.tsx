730c627417c12835e56eec839cccdab9fff2388b
dengwenj <927561237@qq.com>
2023-02-24 21:22:28 +0800

    外出报备

---
+++ src/page/Life/GoOut/GoOutAddEdit.tsx
@@ -0,0 +1,111 @@
+import {
+  ModalForm,
+  ProForm,
+  ProFormDatePicker,
+  ProFormSelect,
+  ProFormText,
+  ProFormTextArea,
+} from '@ant-design/pro-components';
+import { message } from 'antd';
+
+import { getOldpersonByName } from '../../../services/user';
+import { addGoOut, editGoOut } from '../../../services/lift';
+
+import type { StateBool } from '../../../global/types';
+
+interface IGoOutAddEditProps {
+  openGoOutAddEdit: boolean
+  setOpenGoOutAddEdit: StateBool
+  isEdit: boolean
+  record: Record<string, any>
+  reloadPage: () => void
+}
+
+export default function GoOutAddEdit({
+  openGoOutAddEdit,
+  setOpenGoOutAddEdit,
+  isEdit,
+  record,
+  reloadPage
+}: IGoOutAddEditProps) {
+  return (
+    <ModalForm
+      initialValues={record}
+      title={`${isEdit ? '编辑' : '新增'}健康档案`}
+      open={openGoOutAddEdit}
+      modalProps={{
+        destroyOnClose: true,
+        onCancel: () => setOpenGoOutAddEdit(false),
+      }}
+      onFinish={async (values) => {
+        values.oldPersonId = values.oldPersonName.value;
+        delete values.oldPersonName
+
+        if (isEdit) {
+          try {
+            const res = await editGoOut({ id: record.id, ...values });
+            message.success(res.msg);
+            reloadPage();
+            setOpenGoOutAddEdit(false);
+          } catch (error) {
+            console.log(error);
+          }
+        } else {
+          try {
+            const res = await addGoOut(values);
+            message.success(res.msg);
+            reloadPage();
+            setOpenGoOutAddEdit(false);
+          } catch (error) {
+            console.log(error);
+          }
+        }
+        return true;
+      }}
+    >
+      <ProForm.Group>
+        <ProFormSelect.SearchSelect
+          mode='single'
+          width='md'
+          name="oldPersonName"
+          label="姓名"
+          placeholder="请搜索选择姓名"
+          debounceTime={300}
+          request={async ({ keyWords }) => {
+            if (!keyWords) return []
+            const res = await getOldpersonByName(keyWords);
+            return res.data.map((item: any) => {
+              return {
+                value: item.id,
+                label: item.oldPersonName
+              }
+            })
+          }}
+          rules={[{ message: '姓名必选', required: true }]}
+        />
+      </ProForm.Group>
+      <ProForm.Group>
+        <ProFormText
+          width="md"
+          name="goOutEvent"
+          label="外出事件"
+          placeholder="请输入外出事件"
+          rules={[{ message: '外出事件必填', required: true }]}
+        />
+        <ProFormDatePicker
+          width='md'
+          name="goOutTime"
+          label="外出时间"
+          rules={[{ message: '外出时间必选', required: true }]}
+        />
+      </ProForm.Group>
+      <ProFormTextArea
+        width="md"
+        name="goOutAddress"
+        label="外出地址"
+        placeholder="请输入外出地址"
+        rules={[{ message: '外出地址必填', required: true }]}
+      />
+    </ModalForm>
+  )
+}
--- src/page/Life/GoOut/index.tsx
+++ src/page/Life/GoOut/index.tsx
@@ -1,10 +1,213 @@
 /**
  * 外出报备
  */
-import React from 'react'
+/**
+ * 健康档案
+ */
+import { useCallback, useRef, useState } from 'react'
+import { ProTable } from '@ant-design/pro-components';
+import { Button, message, Popconfirm, Space, Tag } from 'antd';
+import { PlusOutlined } from '@ant-design/icons';
+
+import GoOutAddEdit from './GoOutAddEdit';
+import { getOldpersonByName } from '../../../services/user';
+import { pageGoOut, deleteGoOut } from '../../../services/lift';
+
+import type { ActionType, ProColumns } from '@ant-design/pro-components';
 
 export default function GoOut() {
+  const [isShowGoOutAddEdit, setIsShowGoOutAddEdit] = useState(false);
+  const [openGoOutAddEdit, setOpenGoOutAddEdit] = useState(false);
+  const [isEdit, setIsEdit] = useState(false);
+  const [record, setRecord] = useState<Record<string, any>>({});
+
+  const actionRef = useRef<ActionType>();
+
+  const columns: ProColumns[] = [
+    {
+      dataIndex: 'index',
+      valueType: 'indexBorder',
+      width: 48,
+      fixed: 'left',
+    },
+    {
+      title: '姓名',
+      dataIndex: 'oldPersonName',
+      width: 100,
+      fixed: 'left',
+      render(oldPersonName) {
+        return (
+          <Tag color='processing'>{oldPersonName}</Tag>
+        )
+      },
+      valueType: 'select',
+      debounceTime: 300,
+      async request({ keyWords }) {
+        if (!keyWords) return [];
+        const res = await getOldpersonByName(keyWords);
+        return res.data.map((item: any) => {
+          return {
+            value: item.id,
+            label: item.oldPersonName
+          }
+        })
+      },
+      fieldProps(form, config) {
+        return {
+          showSearch: true,
+          placeholder: '请搜索选择'
+        }
+      }
+    },
+    {
+      title: '性别',
+      dataIndex: 'gender',
+      valueEnum: {
+        1: {
+          text: '男',
+          status: 'Success'
+        },
+        0: {
+          text: '女',
+          status: 'Warning',
+        },
+      },
+    },
+    {
+      title: '电话',
+      dataIndex: 'phone',
+      copyable: true,
+      hideInSearch: true
+    },
+    {
+      title: '家属电话',
+      dataIndex: 'familyMemberPhone',
+      copyable: true,
+      hideInSearch: true
+    },
+    {
+      title: '外出事件',
+      dataIndex: 'goOutEvent',
+      hideInSearch: true
+    },
+    {
+      title: '外出时间',
+      dataIndex: 'goOutTime',
+      hideInSearch: true,
+      render(_, record) {
+        return (
+          <>
+            {
+              <Tag color='blue'>
+                {
+                  record.goOutTime
+                }
+              </Tag>
+            }
+          </>
+        )
+      }
+    },
+    {
+      title: '外出地址',
+      dataIndex: 'goOutAddress',
+      ellipsis: true,
+      hideInSearch: true
+    },
+    {
+      title: '操作',
+      hideInSearch: true,
+      width: 150,
+      fixed: 'right',
+      render(_, record) {
+        return (
+          <Space>
+            <a onClick={() => handleAddEdit(true, record)}>
+              编辑
+            </a>
+            <Popconfirm
+              title="确定删除吗？"
+              onConfirm={() => handleDelete(record.id)}
+              okText="确定"
+              cancelText="取消"
+            >
+              <a>删除</a>
+            </Popconfirm>
+
+          </Space>
+        )
+      }
+    }
+  ];
+
+  const reloadPage = useCallback(() => {
+    actionRef.current?.reload();
+  }, []);
+
+  /**
+   * 处理函数
+   */
+  const handleAddEdit = (isEdit: boolean, record: Record<string, any>) => {
+    setIsEdit(isEdit);
+    setRecord(record);
+    setIsShowGoOutAddEdit(true);
+    setOpenGoOutAddEdit(true);
+  }
+
+  // 删除
+  const handleDelete = async (id: number) => {
+    try {
+      const res = await deleteGoOut(id);
+      message.success(res.msg);
+      reloadPage();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
-    <div>GoOut</div>
+    <>
+      <ProTable
+        columns={columns}
+        actionRef={actionRef}
+        request={async (params) => {
+          params.oldPersonId = params.oldPersonName;
+
+          const res = await pageGoOut(params);
+          return {
+            data: res.data,
+            total: res.total,
+            success: true
+          }
+        }}
+        search={{
+          // defaultCollapsed: false
+        }}
+        rowKey="id"
+        headerTitle="外出报备"
+        toolBarRender={() => [
+          <Button
+            icon={<PlusOutlined />}
+            type="primary"
+            onClick={() => handleAddEdit(false, {})}
+          >
+            新建
+          </Button>
+        ]}
+      />
+
+      {/* 新增编辑 */}
+      {
+        isShowGoOutAddEdit && (
+          <GoOutAddEdit
+            openGoOutAddEdit={openGoOutAddEdit}
+            setOpenGoOutAddEdit={setOpenGoOutAddEdit}
+            isEdit={isEdit}
+            record={record}
+            reloadPage={reloadPage}
+          />
+        )
+      }
+    </>
   )
 }
